154d39ba3fe8e135d3bda7e67add658aea893b4a
Ernanni <ernannicf@gmail.com>
2023-12-03 14:22:24 -0300

    citiesList and fix eslint prop rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
   AppLayout,
+  CityList,
   Home,
   Login,
   PageNotFound,
   Pricing,
   Product,
 } from './pages';
+import { useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchCities() {
+    const res = await fetch(`${BASE_URL}/cities`);
+    const data = await res.json();
+    setCities(data);
+  }
+
+  useEffect(() => {
+    try {
+      setIsLoading(true);
+      fetchCities();
+    } catch (error) {
+      alert('There was an error loading data...');
+      console.error(`Please verify if you're runnin the JSON Server`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,8 +43,14 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import AppLayout from './AppLayout/AppLayout';
+import CityList from '../components/CityList/CityList';
 import Home from './Home/Home';
 import Login from './Login/Login';
 import PageNotFound from './PageNotFound/PageNotFound';
 import Pricing from './Pricing/Pricing';
 import Product from './Product/Product';
 
-export { AppLayout, Home, Login, PageNotFound, Pricing, Product };
+export { AppLayout, CityList, Home, Login, PageNotFound, Pricing, Product };
